7f679ce1ffe2460c21fcaf80c032806c795d583e
vvv <vldedov@bk.ru>
2024-04-09 17:11:13 +0500

    Исправлены ошибки в добавлении пользователей.

--- core/database/orm_query.py
+++ core/database/orm_query.py
@@ -5,25 +5,35 @@ from aiogram import types
 
 from core.database.models import Instructions, Problems, User
 
-async def orm_get_all_list(table_name: str, session: AsyncSession):
+def get_model_class_name(table_name: str):
     if table_name == 'instructions':
-        model_class_name = Instructions
-    elif table_name == 'problems':
-        model_class_name = Problems
+        return Instructions
+    return Problems
+
+async def orm_get_all_list(table_name: str, session: AsyncSession):
+    model_class_name = get_model_class_name(table_name)
     query=select(Bundle("lst",model_class_name.name, model_class_name.id, model_class_name.is_published))
     result = await session.execute(query)
     return result.scalars().all()
 
-async def orm_get_row(session: AsyncSession, id: int):
-    query = select(Instructions).where(Instructions.id == id)
+#get one record
+async def orm_get_row(session: AsyncSession, table_name: str, id: int):
+    model_class_name = get_model_class_name(table_name)
+    query = select(model_class_name).where(model_class_name.id == id)
     result = await session.execute(query)
     return result.scalar()
 
 
-async def add_user(message: types.Message, session: AsyncSession):
-    obj = User(
-        user_id=message.from_user.id,
-        name=message.from_user.first_name
-    )
-    session.add(obj)
-    await session.commit()
+async def orm_add_user(message: types.Message, session: AsyncSession):
+    query = select(User.user_id)
+    result = await session.execute(query)
+
+    print(type(message.from_user.id))
+    lst_users = result.scalars().all()
+    if message.from_user.id not in lst_users:
+        obj = User(
+            user_id=message.from_user.id,
+            name=message.from_user.first_name
+        )
+        session.add(obj)
+        await session.commit()
--- core/handlers/user_private.py
+++ core/handlers/user_private.py
@@ -4,7 +4,7 @@ from aiogram.filters import CommandStart, Command, or_f
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.keyboards.inline import START_INLINE_KBD, get_callback_btns
-from core.database.orm_query import orm_get_all_list, add_user
+from core.database.orm_query import orm_get_all_list, orm_add_user, orm_get_row
 
 user_private_router = Router()
 
@@ -12,12 +12,13 @@ user_private_router = Router()
 @user_private_router.message(or_f(CommandStart, F.text.lower().in_({'start', 'начать', 'cтарт'})))
 async def send_main_menu(message: types.Message, session: AsyncSession):
     await message.answer('Выберите из списка ниже, что вы хотите изучить: \n', reply_markup=START_INLINE_KBD)
-    await add_user(message=message, session=session)
+    await orm_add_user(message=message, session=session)
 
 
 @user_private_router.callback_query(F.data.in_({'instructions', 'problems'}))
 async def get_lst_instrustion_problem(callback: types.CallbackQuery, session: AsyncSession):
     answer = await orm_get_all_list(table_name=callback.data, session=session)
+    print(answer)
     dict_btns = dict()
     count = 0
     flag = '_problems'
@@ -37,7 +38,13 @@ async def get_lst_instrustion_problem(callback: types.CallbackQuery, session: As
         await callback.message.answer(text="Список часто задаваемых вопросов:", reply_markup=keyboard)
 
 
-@user_private_router.callback_query(F.data.split('_')[-1] == 'instructions')
-async def show_one_instruction(callback: types.CallbackQuery, session: AsyncSession):
+
+@user_private_router.callback_query(F.data.split('_')[-1].in_({'instructions', 'problems'}))
+async def show_one_problem(callback: types.CallbackQuery, session: AsyncSession):
+    table_name = callback.data.split('_')[-1]
+    id = int(callback.data.split('_')[0])
+    one_row = await orm_get_row(session, table_name, id)
+    print(one_row.id)
+
     await callback.message.answer(text=callback.data.split('_')[0])
     await callback.message.answer('Инструкция!')
